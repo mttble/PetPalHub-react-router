ded49b146c1bcb66de54f1095b2c4fb63181ffb3
mttble <matthew.beale1@hotmail.com>
2023-08-21 17:11:34 +0930

    added a user signup form

--- src/components/SignupUser.jsx
+++ src/components/SignupUser.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './SignupUser.css'
 
 function SignupUser() {
-  // Logic for signup page
+
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [mobile, setMobile] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(email)
+    }
+
     return (
-        <div className="centered-container">
-            <h1>Signup User Page</h1>
-            {/* Add your signup form and content */}
-        </div>
+        <>
+            <div className="centered-container">
+                <h1>Signup User Page</h1>
+            </div>
+            <form onSubmit={ handleSubmit }>
+
+                <label htmlFor="firstName">First name</label>
+                <input value={firstName} firstName="firstName" id="firstName" placeholder="First Name" />
+
+                <label htmlFor="lastName">Last name</label>
+                <input value={lastName} lastName="lastName" id="lastName" placeholder="Last Name" />
+
+                <label htmlFor="mobile">Mobile Number</label>
+                <input value={mobile} mobile="mobile" id="mobile" placeholder="Mobile Number" />
+
+                <label htmlFor="email">email</label>
+                <input value={ email } onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+
+                <label htmlFor="password">password</label>
+                <input value={ pass } onChange={(e) => setPass(e.target.value)} type="password" placeholder="**********" id="password" name="password"/>
+                
+                <button type="submit">Sign Up</button>
+            </form>
+            <button>Already have an account? Login here. </button>
+        </>
     )
 }
 

2e7f7d5873f4d247415fdf96d23a1823a7f6b79b
mttble <matthew.beale1@hotmail.com>
2023-08-21 13:10:18 +0930

    styled homepage and structured navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./NavBar.jsx"
+import './App.css'
 
 
 function App() {
   return(
     <>
       <NavBar />
+      <body>
       <h1>11111</h1>
+      </body>
     </>
   )
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,32 @@
-import Container from 'react-bootstrap/Container';
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import React from 'react';
+import './NavBar.css'
+import Button from 'react-bootstrap/Button';
 
 
 const handleLogin = () => {
-    // Logic to open login modal or redirect to login page
+  // Logic to open login modal or redirect to login page
 };
 
 const handleSignUp = () => {
-    // Logic to open signup modal or redirect to signup page
+  // Logic to open signup modal or redirect to signup page
 };
 
-
-function CollapsibleExample() {
+function ResponsiveNavbar() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Button variant="outline-success" className="mr-2" onClick={handleLogin}>Login</Button>
-            <Button variant="primary" onClick={handleSignUp}>Sign Up</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="navbar">
+      <div className="navbar-brand-container">
+        <div className="navbar-brand">PetPalHub</div>
+      </div>
+      <div className="navbar-buttons">
+        <Button variant="primary" className="size-sm-lg" onClick={handleLogin}>
+          Login
+        </Button>
+        <Button variant="primary" className="size-sm-lg" onClick={handleSignUp}>
+          Signup
+        </Button>
+      </div>
+    </div>
   );
 }
 
-export default CollapsibleExample;
+export default ResponsiveNavbar;

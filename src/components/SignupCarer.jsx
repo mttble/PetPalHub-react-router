2ddac92ad516b3a38584d8a8e131bee7488cee80
mttble <matthew.beale1@hotmail.com>
2023-08-22 16:31:55 +0930

    added backend functionality and error handling for SignupUser

--- src/components/SignupCarer.jsx
+++ src/components/SignupCarer.jsx
@@ -28,14 +28,59 @@ function SignupCarer() {
 
         const {firstName, lastName, phoneNumber, dateOfBirth, country, state, city, street, postalCode, email, password} = data
         try {
+            if (!firstName || !lastName) {
+                toast.error("First name and last name are required.");
+                return;
+            }
+    
+            if (!phoneNumber) {
+                toast.error("Phone number is required.");
+                return;
+            }
+    
+            if (!dateOfBirth) {
+                toast.error("Date of Birth is required.");
+                return;
+            }
+    
+            if (!country) {
+                toast.error("Country is required.");
+                return;
+            }
+    
+            if (!state) {
+                toast.error("State is required.");
+                return;
+            }
+    
+            if (!city) {
+                toast.error("City is required.");
+                return;
+            }
+    
+            if (!street) {
+                toast.error("Street address is required.");
+                return;
+            }
+    
+            if (!postalCode) {
+                toast.error("Postal code is required.");
+                return;
+            }
+    
+            if (!email) {
+                toast.error("Email is required.");
+                return;
+            }
+
             const {data} = await axios.post('http://localhost:5505/register', {firstName, lastName, phoneNumber, dateOfBirth: formattedDob, country, state, city, street, postalCode, email, password
             })
             if(data.error) {
                 toast.error(data.error)
             } else {
                 setData({})
-                toast.success('Register Successful, Weclome!')
-                navigate('/')
+                toast.success('Register Successful, Welcome!')
+                navigate('/login')
             }
         } catch (err) {
             console.log(err)
@@ -48,37 +93,37 @@ function SignupCarer() {
             </div>
             <form className="signup-user-form" onSubmit={ registerCarer }>
 
-                <label htmlFor="firstName">First name</label>
+                <label htmlFor="firstName">First name:</label>
                 <input value={data.firstName} onChange={(e) => setData({...data, firstName: e.target.value})} id="firstName" placeholder="First Name" />
 
-                <label htmlFor="lastName">Last name</label>
+                <label htmlFor="lastName">Last name:</label>
                 <input value={data.lastName} onChange={(e) => setData({...data, lastName: e.target.value})} id="lastName" placeholder="Last Name" />
 
-                <label htmlFor="phoneNumber">Phone Number</label>
+                <label htmlFor="phoneNumber">Phone Number:</label>
                 <input value={data.phoneNumber} onChange={(e) => setData({...data, phoneNumber: e.target.value})} id="mobile" placeholder="Mobile Number" />
 
-                <label htmlFor="dateOfBirth">Date of Birth</label>
+                <label htmlFor="dateOfBirth">Date of Birth:</label>
                 <input value={data.dateOfBirth} onChange={(e) => setData({...data, dateOfBirth: e.target.value})} type="date" id="dateOfBirth" />
                 
-                <label htmlFor="country">Country</label>
+                <label htmlFor="country">Country:</label>
                 <input value={data.country} onChange={(e) => setData({...data, country: e.target.value})} type="text" id="country" placeholder="Country" />
                 
-                <label htmlFor="state">State</label>
+                <label htmlFor="state">State:</label>
                 <input value={data.state} onChange={(e) => setData({...data, state: e.target.value})} type="text" id="state" placeholder="State" />
 
-                <label htmlFor="city">City</label>
+                <label htmlFor="city">City:</label>
                 <input value={data.city} onChange={(e) => setData({...data, city: e.target.value})} type="text" id="city" placeholder="City" />
 
-                <label htmlFor="street">Street</label>
+                <label htmlFor="street">Street:</label>
                 <input value={data.street} onChange={(e) => setData({...data, street: e.target.value})} type="text" id="street" placeholder="Street Address" />
 
-                <label htmlFor="postalCode">Postal Code</label>
+                <label htmlFor="postalCode">Postal Code:</label>
                 <input value={data.postalCode} onChange={(e) => setData({...data, postalCode: e.target.value})} type="text" id="postalCode" placeholder="Postal Code" />
 
-                <label htmlFor="email">Email</label>
+                <label htmlFor="email">Email:</label>
                 <input value={data.email} onChange={(e) => setData({...data, email: e.target.value})} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
 
-                <label htmlFor="password">Password</label>
+                <label htmlFor="password">Password:</label>
                 <input value={data.password} onChange={(e) => setData({...data, password: e.target.value})} type="password" placeholder="**********" id="password" name="password"/>
                 
                 <button type="submit">Sign Up</button>

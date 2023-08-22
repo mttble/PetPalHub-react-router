a7e5b3d1c6179474dd9b08a117ee3798ac66398e
mttble <matthew.beale1@hotmail.com>
2023-08-22 20:23:31 +0930

    added loginUser functionality with backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,25 +1,47 @@
 import React, { useState } from 'react';
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom';
+
 import './Login.css';
 
 
 function Login() {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
+    const navigate = useNavigate()
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    })
 
-    const loginUser = (e) => {
+    const loginUser = async (e) => {
         e.preventDefault()
-        console.log(email)
+        const { email, password } = data
+        try {
+            const {data} = await axios.post('/login', {
+                email,
+                password
+            })
+            if (data.error) {
+                toast.error(data.error)
+            } else {
+                setData({})
+                navigate('/')
+            }
+        } catch (error) {
+            
+        }
     }
+
     return (
         <>
             <div className="centered-container">
                 <h1>Login Page</h1>
             </div>
             <form className="login-form" onSubmit={ loginUser }>
-                <label htmlFor="email">email</label>
-                <input value={ email } onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
-                <label htmlFor="password">password</label>
-                <input value={ pass } onChange={(e) => setPass(e.target.value)} type="password" placeholder="**********" id="password" name="password"/>
+                <label htmlFor="email">email:</label>
+                <input value={ data.email } onChange={(e) => setData({...data, email: e.target.value})} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+                <label htmlFor="password">password:</label>
+                <input value={ password } onChange={(e) => setData({...data, password: e.target.value})} type="password" placeholder="**********" id="password" name="password"/>
                 <button type="submit">Log In</button>
             </form>
         </>

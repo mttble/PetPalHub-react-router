91a16810b08e3cc0a97776e0f7bf580106d1cbb0
mttble <matthew.beale1@hotmail.com>
2023-08-23 18:33:50 +0930

    staged

--- src/components/AccountUser.jsx
+++ src/components/AccountUser.jsx
@@ -1,117 +1,58 @@
-// <<<<<<< main
-// // import React from 'react';
-// // import Button from 'react-bootstrap/Button';
-// // import { Link, Route, Routes } from 'react-router-dom';
-// // import NavBar from './NavBar';
-// // import CreateProfile from './CreateProfile';
-// // import ViewProfile from './ViewProfile';
-// // import './NavBar.css';
-// // import './AccountUser.css';
 
-// // function AccountUser() {
-// //   const hasProfile = true;
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { Link, Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar';
+import CreateProfile from './CreateProfile';
+import ViewProfile from './ViewProfile';
+import './NavBar.css';
+import './AccountUser.css';
 
-// //   return (
-// //     <div>
-// //       <NavBar />
-// //       <div className="centered-container">
-// //         <div className="account-container">
-// //           <h2>Dashboard Overview</h2>
-// //           <div className="account-buttons">
-// //             <Link to="/bookings">
-// //               <Button variant="primary" className="size-sm-lg">
-// //                 Bookings
-// //               </Button>
-// //             </Link>
-// //             <Link to="/petpal-requests">
-// //               <Button variant="primary" className="size-sm-lg">
-// //                 PetPal Requests
-// //               </Button>
-// //             </Link>
-// //             {hasProfile ? (
-// //               <Link to="/view-profile">
-// //                 <Button variant="primary" className="size-sm-lg">
-// //                   View Profile
-// //                 </Button>
-// //               </Link>
-// //             ) : (
-// //               <Link to="/create-profile">
-// //                 <Button variant="primary" className="size-sm-lg">
-// //                   Create Profile
-// //                 </Button>
-// //               </Link>
-// //             )}
-// //           </div>
-// //           <Routes>
-// //             <Route
-// //               path="/create-profile"
-// //               element={<CreateProfile onCreateProfile={handleCreateProfile} />}
-// //             />
-// //             <Route path="/view-profile" element={<ViewProfile />} />
-// //           </Routes>
-// //         </div>
-// //       </div>
-// //     </div>
-// //   );
-// // }
+function AccountUser() {
+  const hasProfile = true;
 
-// // export default AccountUser;
-// =======
-// import React from 'react';
-// import Button from 'react-bootstrap/Button';
-// import { Link, Route, Routes } from 'react-router-dom';
-// import NavBar from './NavBar';
-// import CreateProfile from './CreateProfile';
-// import ViewProfile from './ViewProfile';
-// import './NavBar.css';
-// import './AccountUser.css';
+  return (
+    <div>
+      <NavBar />
+      <div className="centered-container">
+        <div className="account-container">
+          <h2>Dashboard Overview</h2>
+          <div className="account-buttons">
+            <Link to="/bookings">
+              <Button variant="primary" className="size-sm-lg">
+                Bookings
+              </Button>
+            </Link>
+            <Link to="/petpal-requests">
+              <Button variant="primary" className="size-sm-lg">
+                PetPal Requests
+              </Button>
+            </Link>
+            {hasProfile ? (
+              <Link to="/view-profile">
+                <Button variant="primary" className="size-sm-lg">
+                  View Profile
+                </Button>
+              </Link>
+            ) : (
+              <Link to="/create-profile">
+                <Button variant="primary" className="size-sm-lg">
+                  Create Profile
+                </Button>
+              </Link>
+            )}
+          </div>
+          <Routes>
+            <Route
+              path="/create-profile"
+              element={<CreateProfile onCreateProfile={handleCreateProfile} />}
+            />
+            <Route path="/view-profile" element={<ViewProfile />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-// function AccountUser() {
-//   const hasProfile = true;
-
-//   return (
-//     <div>
-//       <NavBar />
-//       <div className="centered-container">
-//         <div className="account-container">
-//           <h2>Dashboard Overview</h2>
-//           <div className="account-buttons">
-//             <Link to="/bookings">
-//               <Button variant="primary" className="size-sm-lg">
-//                 Bookings
-//               </Button>
-//             </Link>
-//             <Link to="/petpal-requests">
-//               <Button variant="primary" className="size-sm-lg">
-//                 PetPal Requests
-//               </Button>
-//             </Link>
-//             {hasProfile ? (
-//               <Link to="/view-profile">
-//                 <Button variant="primary" className="size-sm-lg">
-//                   View Profile
-//                 </Button>
-//               </Link>
-//             ) : (
-//               <Link to="/create-profile">
-//                 <Button variant="primary" className="size-sm-lg">
-//                   Create Profile
-//                 </Button>
-//               </Link>
-//             )}
-//           </div>
-//           <Routes>
-//             <Route
-//               path="/create-profile"
-//               element={<CreateProfile onCreateProfile={handleCreateProfile} />}
-//             />
-//             <Route path="/view-profile" element={<ViewProfile />} />
-//           </Routes>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default AccountUser;
-// >>>>>>> main
+export default AccountUser;

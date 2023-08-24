c15f1cfcf384a5d1a737d658ba1145dd5c25ceb8
mttble <matthew.beale1@hotmail.com>
2023-08-24 10:46:59 +0930

    added 'loading' displayed to user after login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,30 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../Context/userContext';
 
 import './Login.css';
 
 
 function Login() {
     const navigate = useNavigate();
+    const {setUser} = useContext(UserContext)
     const [data, setData] = useState({
         email: '',
         password: '',
     });
+    const [isLoading, setIsLoading] = useState(false)
 
     const loginUser = async (e) => {
         e.preventDefault();
         const { email, password } = data;
+        setIsLoading(true)
         try {
             const response = await axios.post('/login', {
                 email,
                 password
             });
-
+            setUser(response.data)
             if (response.data.error) {
                 toast.error(response.data.error);
             } else {
+                toast.success('Logged in successfully')
                 setData({});
                 navigate('/dashboard');
             }
@@ -35,6 +40,7 @@ function Login() {
                 toast.error('An error occurred while logging in.');
             }
         }
+        setIsLoading(false)
     };
 
     return (
@@ -47,7 +53,7 @@ function Login() {
                 <input value={ data.email } onChange={(e) => setData({...data, email: e.target.value})} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
                 <label htmlFor="password">password:</label>
                 <input value={ data.password } onChange={(e) => setData({...data, password: e.target.value})} type="password" placeholder="**********" id="password" name="password"/>
-                <button type="submit">Log In</button>
+                <button type="submit" disabled={isLoading}>{isLoading ? 'Logging in...' : 'Log In'}</button>
             </form>
         </>
     );

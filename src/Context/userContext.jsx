ebe3d3700a84abe34fbbd6da32df70b04fa92f5b
mttble <matthew.beale1@hotmail.com>
2023-09-02 15:02:25 +0930

    debugging cookie removal

--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -10,15 +10,26 @@ export const UserContextProvider = ({ children }) => {
     const navigate = useNavigate()
     const [user, setUser] = useState(null);
 
+    const expireCookies = (cookieNames) => {
+        const expirationDate = new Date(0).toUTCString(); // Set expiration to the past
+    
+        for (const cookieName of cookieNames) {
+            document.cookie = `${cookieName}=; expires=${expirationDate}; path=/; secure; sameSite=none`;
+            console.log(`Cookie '${cookieName}' removed`);
+        }
+    };
+    
     const logout = () => {
         // Clear user data and cookies
         setUser(null);
         localStorage.removeItem('userData');
     
-        // Remove the cookies
-        document.cookie = "carerToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; sameSite=none";
-        document.cookie = "userToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; sameSite=none";
-        }
+        // Specify the cookie names to expire
+        const cookieNames = ['carerToken', 'userToken'];
+        
+        // Call the function to expire cookies
+        expireCookies(cookieNames);
+        
         navigate('/');
     };
 

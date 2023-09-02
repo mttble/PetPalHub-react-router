06d1b7e0be2e60a87b663fcc414fc1e9e66863eb
mttble <matthew.beale1@hotmail.com>
2023-09-02 19:30:43 +0930

    redo

--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -10,18 +10,8 @@ export const UserContextProvider = ({ children }) => {
     const navigate = useNavigate()
     const [user, setUser] = useState(null);
 
-    function deleteAllCookies() {
-        // Get all cookies for the current domain
-        const cookies = document.cookie.split(';');
+
     
-        // Iterate through cookies and delete each one
-        for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i];
-          const eqPos = cookie.indexOf('=');
-          const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-          document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-        }
-    }
     const logout = () => {
         // Clear user data and cookies
         setUser(null);
@@ -34,7 +24,6 @@ export const UserContextProvider = ({ children }) => {
             document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/; secure; sameSite=none`;
             console.log('cookie removed')
         }
-        deleteAllCookies()
         navigate('/');
     };
 

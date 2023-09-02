36cb8951d5eca2234701ca0f29753e54e9cb6f25
mttble <matthew.beale1@hotmail.com>
2023-09-02 12:38:30 +0930

    fix

--- src/Context/userContext.jsx
+++ src/Context/userContext.jsx
@@ -25,7 +25,13 @@ export const UserContextProvider = ({ children }) => {
         navigate('/');
     };
 
-
+    useEffect(() => {
+        if (!user)  {
+            axios.get('/profile').then(({data}) => {
+                setUser(data)
+            })
+        }
+    }, []);
 
 
     return (
